f409074e456954ec1885dee113118ed281491aa6
Javarn15 <javarnray10@gmail.com>
2026-03-09 14:48:24 +1300

    added if function for validation

--- internalprogram.py
+++ internalprogram.py
@@ -11,3 +11,8 @@ quantities = []
 return_dates=[]
 
 # ---------------- VALIDATION FUNCTION ----------------
+def check_input(name, receipt, item, quantity, return_date):
+    # check empty fields
+    if name == "" or receipt == "" or item == "" or quantity == "" or return_date == "":
+        messagebox.showerror("Input Error", "All fields must be filled in")
+        return False
